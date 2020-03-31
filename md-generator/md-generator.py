24b72e13ebe13a74802077098009cd948e99d4cd
Nicolás Patarino <npatarino@gmail.com>
2020-03-31 11:55:41 +0200

    New vresion of generator

--- md-generator/md-generator.py
+++ md-generator/md-generator.py
@@ -6,22 +6,19 @@ import sys
 PIPE = "|"
 SLASH = "/"
 PNG_FOLDER = "PNG"
+SVG_FOLDER = "SVG"
+PDF_FOLDER = "PDF"
 PNG_EXTENSION = ".png"
+SVG_EXTENSION = ".svg"
+PDF_EXTENSION = ".pdf"
 BREAK = "\n"
-
-
-class Icon:
-    def __init__(self, image, name):
-        self.image = image
-        self.name = name
-
 
 if __name__ == '__main__':
     path = sys.argv[1]
     files = os.listdir(path)
     root = os.path.basename(path)
     dictionary = {}
-    file_content = ""
+    file_content = "| global | name | SVG | PDF | | O2 | name | SVG | PDF |" + BREAK + "| - | :-: | - | :-: | - | - | - | - | - |" + BREAK
     for brand in files:
         brand_folder = path + SLASH + brand
         if os.path.isdir(brand_folder) and brand != ".DS_Store":
@@ -32,17 +29,20 @@ if __name__ == '__main__':
                         brands.append(brand)
                     else:
                         dictionary[icon] = [brand]
+
     for icon in dictionary:
         line_image = PIPE
         line_separator = BREAK + PIPE
         line_name = BREAK + PIPE
+        row = ""
+        counter = 0
+        dictionary[icon].sort()
         for brand in dictionary[icon]:
-            image = "[![" + icon + "](" + root + SLASH + brand + SLASH + icon + SLASH + PNG_FOLDER + SLASH + icon + PNG_EXTENSION + ")](" + root + SLASH + brand + SLASH + icon + SLASH + ")"
-            line_image = line_image + image + PIPE
-            line_separator = line_separator + " :----------: " + PIPE
-            line_name = line_name + icon + PIPE
-        line = line_image + line_separator + line_name + BREAK + BREAK
-        file_content += line
+            path_icon = root + SLASH + brand + SLASH + icon + SLASH + PNG_FOLDER + SLASH + icon + PNG_EXTENSION
+            svg_icon = root + SLASH + brand + SLASH + icon + SLASH + SVG_FOLDER + SLASH + icon + SVG_EXTENSION
+            pdf_icon = root + SLASH + brand + SLASH + icon + SLASH + PDF_FOLDER + SLASH + icon + PDF_EXTENSION
+            row = row + "| ![" + icon + "](" + path_icon + ") | `" + icon + "`  |  [.svg](" + svg_icon + ") | [.pdf](" + pdf_icon + ") |  "
+        file_content += row + BREAK
 
     output_file_path = os.path.dirname(path) + SLASH + "README.md"
     print(output_file_path)
